3aba4c47429695e9b9292cc5139c6b90d8320600
nicole-avila <114564720+nicole-avila@users.noreply.github.com>
2024-02-02 13:26:55 +0100

    creating + import components to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.scss";
+import Form from "./components/Form/Form";
+import Header from "./components/Header/Header";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <h1>app</h1>
+    <div className="app">
+      <div className="app__header">
+        <Header />
+      </div>
+      <div className="app__form">
+        <Form />
+      </div>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import "./Footer.scss";
+
+export default function Footer() {
+  return (
+    <footer>
+      <h1>footer</h1>
+    </footer>
+  );
+}
